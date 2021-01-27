06144b41b01790fb6afd6d62361485eec39712c7
alak <alak@estatelogs.com>
2021-01-27 14:56:46 +0200

    ...

--- App.tsx
+++ App.tsx
@@ -5,12 +5,13 @@ import PressableButton from "./Button";
 import axios from "axios";
 const api = "https://jsonplaceholder.typicode.com/users";
 const anApi = "https://jsonplaceholder.typicode.com/photos";
-
-
+const pokeMonApi = 'https://pokeapi.co/api/v2/pokemon?limit=500'
+const urlImage = 'https://pokeres.bastionbot.org/images/pokemon/';
 export default function App() {
   const [data, setData] = React.useState([]);
   const [anotherdata, setanotherData] = React.useState([]);
   const [selectedTab, setSelectedTab] = React.useState(0);
+  const [Pokemon, setPokemon] = React.useState([]);
 
   const updateState = async () => {
     setSelectedTab(0);
@@ -34,6 +35,17 @@ export default function App() {
       });
   };
 
+  const PokemonState = async () => {
+    setSelectedTab(2);
+    await axios(pokeMonApi)
+      .then((res) => {
+        setPokemon(res.data.results);
+      })
+      .catch((err) => {
+        console.log("failed to catch", err);
+      });
+  };
+
   return (
     <React.Fragment>
       <Container>
@@ -48,7 +60,7 @@ export default function App() {
           bgColor="lightblue"
         />
         <PressableButton
-          onPress={() => true}
+          onPress={PokemonState}
           title="Third button"
           bgColor="#4267B2"
         />
@@ -81,6 +93,32 @@ export default function App() {
             />
           )}
       </View>
+      <View style={{ display: selectedTab === 2 ? "flex" : "none" }}>
+        <Text style={{ fontSize: 24, fontWeight: "bold" }}>
+          Third Button
+          </Text>
+        {Pokemon && Pokemon === undefined ? (
+          <Text>loading</Text>
+        ) : (
+            <FlatList
+              numColumns={2}
+              data={Pokemon}
+              renderItem={({ item }) => {
+                let url = item.url;
+                const idPokemon = url.split('https://pokeapi.co/api/v2/pokemon/');
+                const link = urlImage + idPokemon[1].substring(0, idPokemon[1].length - 1) + ".png";
+                return <Item>
+                  <Image
+                    resizeMode="contain"
+                    source={{ uri: link }}
+                  />
+                  <TextName>{item.name}</TextName>
+                </Item>
+              }}
+              keyExtractor={item => `key-${item.name}`}
+            />
+          )}
+      </View>
 
     </React.Fragment>
   );
@@ -92,6 +130,25 @@ const Container = styled.View`
   padding: 70px 0px 20px 0px;
 `;
 
-const Scroll = styled.ScrollView`
-  flex: 1;
+const Image = styled.Image`
+width: 100px;
+height: 100px;
 `;
+
+const Item = styled.View`
+padding:4px;
+flex: 1;
+justify-content: center;
+align-items: center;
+background-color: white;
+margin-top: 8px;
+margin-horizontal: 5px;
+shadowColor: #000;
+shadow-opacity: 0.25;
+shadow-radius: 3.84px;
+elevation: 5;
+`
+const TextName = styled.Text`
+color: orange;
+font-weight: bold;
+`
